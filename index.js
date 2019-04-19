bbd0383bbabe759a0d1cb756e0c87acdf36e3134
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2019-04-19 18:29:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,16 +36,4 @@ bot.on('message', message => {
 	}
 })
 
-bot.on('message', message => {
-    let args = message.content.substring(PREFIX.length).split(" ");
-
-    switch(args[0]{
-        case 'ping':
-            message.channel.sendmessage('pong!')
-        break;
-        case 'clear':
-            if(!args[1]) return message.reply('Please specify the number of messages to delete!')
-            message.channel.bulkDelete(args[1];
-        break;
-
 bot.login(process.env.token);
