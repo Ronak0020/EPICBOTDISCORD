60f57e1ecc91ef8202d4d1698fde7cb0b0b81f8f
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2019-04-19 18:21:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+const PREFIX = ';';
+
 bot.on('ready', async () => {
 	console.log('${bot.user.username} is online!');
 	bot.user.setActivity("Made by Ronak!", ("type: PLAYING"));
@@ -33,5 +35,17 @@ bot.on('message', message => {
 		message.channel.send('https://cdn.discordapp.com/attachments/547949244686794782/566933965521289226/DialgaVSpalkia_hq.gif')
 	}
 })
+
+bot.on('message', message => {
+let args = message.content.substring(PREFIX.length).split(" ");
+
+switch(args[0]{
+case 'ping':
+message.channel.sendmessage('pong!')
+break;
+case 'clear':
+if(!args[1]) return message.reply('Please specify the number of messages to delete!')
+message.channel.bulkDelete(args[1];
+break;
 
 bot.login(process.env.token);
