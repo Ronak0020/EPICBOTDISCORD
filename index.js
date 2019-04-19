008b17a1de565a6d62e6393fc8a3c40678b98efb
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2019-04-19 17:21:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const config = require("./config.json")
+const config = require("BOT_TOKEN")
 
 bot.on('ready', async () => {
 	console.log('${bot.user.username} is online!');
