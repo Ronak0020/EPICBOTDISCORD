31fee1bbfac71b09684985206bad9ac7e1c4d03a
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2019-04-24 10:46:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ bot.on('message', message => {
 			message.channel.sendEmbed(USER);
 		break;
 		case 'info':
+			if(message.author.bot) return;
 			message.channel.send('Please specify what info you want? `1. Bot, 2. Developer, 3. Server, 4. Commands` (type ;info <type of info>')
 		        if(args[1] === 'bot'){
 			message.channel.send('__***Hi there! I am Epic Poke bot! I was cretaed for Epic poke server! I can manage the server and i can do things that requires `ADMINISTRATIVE PERMISSIONS`! I AM STILL IN DEVELOPMENT!!***__')	
