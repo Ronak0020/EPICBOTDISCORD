b656b391ce3feb8ddef10ece39060f3e59ea8ac7
Ronak0020 <khandelwalsakshi1810@gmail.com>
2019-05-08 20:53:16 +0530

    oof

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ bot.on('message', message => {
 		   if(!args[1]) return message.reply('Please specify thr number of messages to delete!')
 		   if(!message.member.hasPermission(['MANAGE_MESSAGES'])) return message.reply('You do not have permission to use this command!')
 		   message.channel.bulkDelete(args[1])
-		   message.channel.send(`deleted ***${args[1]}*** messages!`)
+		   message.channel.send(`deleted __***${args[1]}***__ messages!`)
 		   message.delete()
 	        break;
 	                }
