8af51987c68036951ca853fd50774388fd2a9279
Ronak0020 <khandelwalsakshi1810@gmail.com>
2019-06-01 14:51:07 +0530

    ronak

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ bot.on('message', message => {
 				message.author.send(`${message.author} You have sent your message to ${dUser}`)
 		break;
 		case 'role':
-			var role = message.guild.roles.find(role => role.name === "test");
+			var role = message.guild.roles.find(role => role.name === "New poké trainer") || (role => role.name === "∴━━━✿━━━∴ [ Level Roles ] ∴━━━✿━━━∴");
 			message.member.addRole(role)
 			message.author.send('Thanks for verifying!');
 	                }
