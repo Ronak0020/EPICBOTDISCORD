653ee09726ad086d06903da89db9b485d3312256
Ronak0020 <khandelwalsakshi1810@gmail.com>
2019-10-06 16:11:24 +0530

    dm

--- index.js
+++ index.js
@@ -174,9 +174,8 @@ bot.on('message', message => {
 			case 'dmu':          
 				let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 				if (!dUser) return message.channel.send("Can't find user!")
-				if(!message.member.hasPermission(['ADMINISTRATOR'])) return message.reply("You can't you that command!")
+				if(!message.member.hasPermission(['ADMINISTRATOR'])) return message.reply("You can't use that command!")
 				let dMessage = args.join(" ").slice(22);
-				if(dMessage.length < 1) return message.reply('You must supply a message!')
 		
 				dUser.send(`${dMessage}`)
 		
