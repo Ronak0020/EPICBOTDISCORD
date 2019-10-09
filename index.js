8b1500db11537946a45997b8c503e140d6a925c8
Ronak0020 <khandelwalsakshi1810@gmail.com>
2019-10-09 14:03:57 +0530

    trial get list

--- index.js
+++ index.js
@@ -182,18 +182,18 @@ bot.on('message', message => {
 				message.author.send(`${message.author} You have sent your message to ${dUser.id}`)
 		break;
 		case 'slap':
+				let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+				let author = message.author;
+				let slap_cmd = {
+					file: slap_img[Math.floor(Math.random() * slap_img.length)]
+				}
 				const slap_img = [
                                 'https://discordapp.com/channels/626097765776097310/626288664330371083/630711309503168522', 'https://discordapp.com/channels/626097765776097310/626288664330371083/630711346370969616', 'https://discordapp.com/channels/626097765776097310/626288664330371083/630711355942633484', 'https://discordapp.com/channels/626097765776097310/626288664330371083/630711390419681290'
 							]
 				if(!args[1]) return message.reply('Please mention a valid user to Slap!')
 				const Slap = new RichEmbed()
-				let slap_cmd = {
-					file: slap_img[Math.floor(Math.random() * slap_img.length)]
-				}
-				let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-				let author = message.author
-   				.addField(`Ouch! ${author} slapped ${user}! It hurts! :sob: `)
-				.addField(message.channel.send(slap_cmd))
+				.addField(`Ouch! ${author} slapped ${user}! It hurts! :sob: `)
+				.setImage(slap_cmd)
 				.setColor(0xFCA00F)
 				message.channel.send(Slap)
 		break;		
