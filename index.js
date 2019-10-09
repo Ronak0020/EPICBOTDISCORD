f90b7abc52f7ecc6be3d9192280744600b21d785
Ronak0020 <khandelwalsakshi1810@gmail.com>
2019-10-09 14:50:22 +0530

    sojv

--- index.js
+++ index.js
@@ -186,11 +186,9 @@ bot.on('message', message => {
 				let author = message.author;
 				if(!args[1]) return message.reply('Please mention a valid user to Slap!')
 				const slap1 = new Attachment('./slap_images/slap1.gif', 'slap1.gif');
-				const slap_img = {
-					number = 4,
+				const slap_img = number = 4;
 					imageNumber = Math.floor (Math.random() * number) + 1
-					({files: ["./slap_images/" + imageNumber + ".gif"]})
-				}
+					({files: ["./slap_images/" + imageNumber + ".gif"]});
 				const Slap = new RichEmbed()
 				.attachFile(slap1)
 				.setImage('attachment://slap1.gif')
