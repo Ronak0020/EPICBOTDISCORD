563a1555df3ba23f9ea4ddf76cd3a4fb5d960628
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2019-10-09 18:34:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ bot.on('message', message => {
 				let use = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 				let auth = message.author;
 				if(!args[1]) return message.reply('Please mention a valid user to hug!')
-				const hug1 = new Attachment('./tenor(5).gif', 'tenor (5).gif');
+				const hug1 = new Attachment('./tenor (5).gif', 'tenor (5).gif');
 				const Hug = new RichEmbed()
 				.attachFile(hug1)
 				.setImage('attachment://tenor (5).gif')
